77225b420cce76b569d21357332b4858c882b67b
David <dvlewicki@gmail.com>
2014-02-19 20:14:11 +0000

    Add student_id to attendances

---
+++ db/migrate/20140219201222_student_id_to_attendances.rb
@@ -0,0 +1,5 @@
+class StudentIdToAttendances < ActiveRecord::Migration
+  def change
+    add_column :attendances, :student_id, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140214011000) do
+ActiveRecord::Schema.define(version: 20140219201222) do
 
   create_table "attendances", force: true do |t|
     t.string   "attended_on"
     t.integer  "seat_number"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "student_id"
   end
 
   create_table "comments", force: true do |t|
